6f92c4f4f61f99f3987b6add12d29fc3bdde5a5c
Eric Hodges <ehodges2004@gmail.com>
2018-07-02 14:22:45 -0700

    added new photo and moved background image css to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="div-with-background">
     <Nav />
     <nuxt/>
     <Footer />
@@ -20,3 +20,15 @@
     }
   }
 </script>
+
+<style lang="scss">
+  .div-with-background {
+    height: 100vh; 
+    background-image: linear-gradient(
+      to bottom, rgba($color-grey-light-1, .3),
+      rgba($color-grey-light-2, .9)), url('~/assets/images/stock-photo-woman-lying-in-bed-sleepless-at-night-223663354.jpg');      
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+  }
+</style>

f8666735928cea486dfe2430817b2e822d0f3b5f
Eric Hodges <14981329+erichodges@users.noreply.github.com>
2018-08-13 14:23:53 -0700

    deleted unused plugins

--- plugins/pp-scroll.js
+++
@@ -1,5 +0,0 @@
-import Vue from "vue";
-import vuePreventParentScroll from "vue-prevent-parent-scroll";
-
-Vue.use(vuePreventParentScroll)
-
--- plugins/scroll-lock.js
+++
@@ -1,4 +0,0 @@
-import Vue from "vue";
-import VueScrollLock from "vue-scroll-lock";
-
-Vue.use(VueScrollLock);
